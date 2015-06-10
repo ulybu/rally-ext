05a389253db14163c1616dc627a88108a1653865
Ulysse Prygiel <ulysse.prygiel@hp.com>
2015-06-10 11:57:23 +0200

    refactoring. - init function - internal conf and user conf (pre-requisite to a setting page) - the icon is now cloned from a template

--- inject-clips.js
+++ inject-clips.js
@@ -1,78 +1,97 @@
-window.uly = {};
-uly.iconClass = 'uly-clip-icon';
-uly.fontUrl = chrome.extension.getURL('icons/octicons.woff');
+window.uly = {
+  userConf : {
+    injectionInterval : 4000,
+    checkMarkFadeAwayDelay : 2000
+  },
+  _conf : {
+    iconClass: 'uly-clip-icon',
+    farOnTopClass: 'uly-farOnTop',
+    fontUrl: '',
+    injectedFlagClass: 'uly-injected'
+  },
+  init : function() {
+    this._conf.fontUrl = chrome.extension.getURL('icons/octicons.woff');
 
-var fontFaceStyle = document.createElement('style');
-fontFaceStyle.appendChild(document.createTextNode("\
-@font-face {\
-    font-family: 'octicons';\
-    src: url('" + uly.fontUrl + "') format('woff');\
-}\
-"));
-document.head.appendChild(fontFaceStyle);
+    this.addFontFace(this._conf.fontUrl);
 
-// Dummy element to use as a text buffer
-uly.p = document.createElement("p");
-uly.p.classList.add('uly-farOnTop');
-document.body.appendChild(uly.p); // needs to have a parent
-// Dummy range to use as a text buffer (also)
-uly.range = document.createRange();
-uly.range.selectNode(uly.p);
+    this.p = document.createElement("p");
+    this.p.classList.add(this._conf.farOnTopClass);
+    document.body.appendChild(this.p); // needs to have a parent
+    // Dummy range to use as a text buffer (also)
+    this.range = document.createRange();
+    this.range.selectNode(this.p);
 
-/**
- * Handler to the click on the paperclip
- * @param {[Object event]} e The dom event object
- */
-uly.copyToClipb = function(e) {
-  // get the text from the sibling link
-  var icon = e.target//
-    , url = icon.parentElement.querySelector('.formatted-id-link').href
-    , succeeded = true
-    , whichIcon
-    ;
-  uly.p.textContent = url;
-  window.getSelection().addRange(uly.range);
+    this.iconTemplate = this.makeIconElemTemplate();
+  },
+  addFontFace : function(fontUrl) {
+    var fontFaceStyle = document.createElement('style');
+    fontFaceStyle.appendChild(document.createTextNode("\
+    @font-face {\
+        font-family: 'octicons';\
+        src: url('" + fontUrl + "') format('woff');\
+    }\
+    "));
+    document.head.appendChild(fontFaceStyle);
+  },
+  makeIconElemTemplate : function() {
+    var tempNode = document.createElement("span");
+    tempNode.classList.add(this._conf.iconClass);
+    tempNode.classList.add('octicon-clippy');
+    tempNode.classList.add('octicon');
+    return tempNode;
+  },
+  copyToClipb : function(e) {
+    // get the text from the sibling link
+    var icon = e.target
+      , url = icon.parentElement.querySelector('.formatted-id-link').href
+      , succeeded = true
+      , whichIcon
+      ;
+    this.p.textContent = url;
+    window.getSelection().addRange(this.range);
 
-  try {
-    // Now that we've selected the url, execute the copy command
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Copy US/Task was ' + msg);
-    // console.log("after try");
-  } catch(err) {
-    succeeeded = false;
-    console.log('Oops, unable to copy');
-  } finally {
-    whichIcon = succeeded ? 'octicon-check' : 'octicon-x';
-    icon.classList.add(whichIcon);
-    icon.classList.remove('octicon-clippy');
-    setTimeout(function(){
-      icon.classList.remove(whichIcon);
-      icon.classList.add('octicon-clippy');
-    },2000);
+    try {
+      // Now that we've selected the url, execute the copy command
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'successful' : 'unsuccessful';
+      console.log('Copy US/Task was ' + msg);
+      // console.log("after try");
+    } catch(err) {
+      succeeeded = false;
+      console.log('Oops, unable to copy');
+    } finally {
+      whichIcon = succeeded ? 'octicon-check' : 'octicon-x';
+      icon.classList.add(whichIcon);
+      icon.classList.remove('octicon-clippy');
+      setTimeout(function(){
+        icon.classList.remove(whichIcon);
+        icon.classList.add('octicon-clippy');
+      },this.userConf.checkMarkFadeAwayDelay);
+    }
+  },
+  injectLinks : function() {
+    var injectedClass = this._conf.injectedFlagClass
+    var links = document.querySelectorAll(".formatted-id-template:not(."+injectedClass+")");
+    // Not very performance friendly, but currently got now way
+    // to know when links are created (after async calls to a back-end)
+    if ( links.length > 0 ) {
+      console.log('Found '+links.length+' links without copy-to-clip button');
+    }
+
+    for (var i=0, link, iconElem; (link=links[i]); i++) {
+      link.classList.add(injectedClass);
+      iconElem = this.iconTemplate.cloneNode();
+      iconElem.addEventListener('click', this.copyToClipb.bind(this));
+      link.parentElement.appendChild(iconElem);
+    }
+  },
+  startInjecting : function() {
+    this.intervalID = setInterval(this.injectLinks.bind(this), this.userConf.injectionInterval);
+  },
+  stopInjecting : function() {
+    clearInterval(this.intervalID);
   }
-}
+};
 
-uly.injectLinks = function() {
-  uly.links = document.querySelectorAll(".formatted-id-template:not(.uly-injected)");
-  // Not very performance friendly, but currently got now way
-  // to know when links are created (after async calls to a back-end)
-  if ( uly.links.length > 0 ) {
-    console.log('Found '+uly.links.length+' links without copy-to-clip button');
-  }
-
-  for (var i=0, link, iconElem; (link=uly.links[i]); i++) {
-    iconElem = document.createElement("span");
-    iconElem.classList.add(uly.iconClass);
-    iconElem.classList.add('octicon-clippy');
-    iconElem.classList.add('octicon');
-    iconElem.addEventListener('click', uly.copyToClipb);
-    link.parentElement.appendChild(iconElem);
-  }
-}
-
-uly.areClipsInjected = function() {
-  return document.querySelectorAll('.' + uly.iconClass).length !== 0;
-}
-
-uly.intervalId = setInterval(uly.injectLinks, 5000);
+uly.init();
+uly.startInjecting();
