32c9b1760f4e25c01ab14a9b816d5d353a17263c
Ulysse Prygiel <ulysse.prygiel@hp.com>
2015-06-17 17:16:08 +0200

    Add folder structure and build for the option page

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ var gulp = require('gulp'),
   copy = require('gulp-copy'),
   zip = require('gulp-zip'),
   sync = require('gulp-config-sync'),
-  del = require('del')
+  del = require('del'),
+  polybuild = require('polybuild')
   ;
 var zipName = 'rally-ext.zip';
 var syncOptions = {
@@ -17,6 +18,22 @@ var syncOptions = {
   ],
   space: '  ',
 };
+
+gulp.task('build-wc', ['clean'], function() {
+  return gulp.src('src/options/options.html')
+  .pipe(polybuild({
+    maximumCrush: true
+  }))
+  .pipe(gulp.dest('build/src/options'))
+;
+});
+gulp.task('rename-wc', ['build-wc'], function() {
+  return gulp.src('build/src/options/options.build.html')
+  .pipe(rename("build/src/options/options.html"))
+  .pipe(gulp.dest('.'))
+;
+});
+
 gulp.task('sync-packages', function() {
   return gulp.src(['bower.json', 'package.json'])
     .pipe(sync(syncOptions))
@@ -39,5 +56,5 @@ gulp.task('zip', ['build'], function() {
     .pipe(zip(zipName))
     .pipe(gulp.dest('.'));
 })
-gulp.task('build', ['build-injection','copy-non-minified']);
+gulp.task('build', ['build-injection','copy-non-minified','build-wc','rename-wc']);
 gulp.task('default',['build']);
