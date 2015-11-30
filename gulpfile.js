c5f1ef33d3c411e36304573f3f46593c7d52ddae
Ulysse Prygiel <ulysse.prygiel@gmail.com>
2015-11-30 02:29:25 +0100

    sperate options css and refactor gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp'),
+  merge = require('merge-stream'),
   uglify = require('gulp-uglify'),
   rename = require('gulp-rename'),
   notify = require('gulp-notify'),
   copy = require('gulp-copy'),
   zip = require('gulp-zip'),
   sync = require('gulp-config-sync'),
-  del = require('del'),
+  del = require('del')
   ;
 var zipName = 'rally-ext.zip';
 var syncOptions = {
@@ -27,18 +28,24 @@ gulp.task('clean',function(cb) {
   del(['build',zipName], cb);
 })
 gulp.task('copy-non-minified', ['clean'], function() {
-  return gulp.src(['manifest.json','icons/*', 'src/injection/*.css',"src/shared/*.js", 'src/options/options.html'])
+  return gulp.src(['manifest.json','icons/*',"src/shared/*.js"])
     .pipe(copy('build'));
 })
 gulp.task('build-injection', ['clean'], function() {
-  return gulp.src('src/injection/*.js')
+  var js = gulp.src('src/injection/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('build/src/injection'));
+  var css =  gulp.src(['src/injection/*.css'])
+    .pipe(copy('build'));
+    return merge(js,css);
 });
 gulp.task('build-options', ['clean'], function() {
-  return gulp.src('src/options/*.js')
+  var js = gulp.src('src/options/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('build/src/options'));
+  var misc = gulp.src(['src/options/options.html', 'src/options/options.css'])
+    .pipe(copy('build'));
+    return merge(js,misc);
 });
 gulp.task('zip', ['build'], function() {
   return gulp.src('build/**')
