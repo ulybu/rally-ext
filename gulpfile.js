dd966bb67101bf7b52efa60be7a38e191f509862
Ulysse Prygiel <ulysse.prygiel@gmail.com>
2015-11-30 01:57:46 +0100

    build with the options UI

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,6 @@ var gulp = require('gulp'),
   zip = require('gulp-zip'),
   sync = require('gulp-config-sync'),
   del = require('del'),
-  polybuild = require('polybuild')
   ;
 var zipName = 'rally-ext.zip';
 var syncOptions = {
@@ -19,21 +18,6 @@ var syncOptions = {
   space: '  ',
 };
 
-gulp.task('build-wc', ['clean'], function() {
-  return gulp.src('src/options/index.html')
-  .pipe(polybuild({
-    maximumCrush: true
-  }))
-  .pipe(gulp.dest('build/src/options'))
-;
-});
-gulp.task('rename-wc', ['build-wc'], function() {
-  return gulp.src('build/src/options/index.build.html')
-  .pipe(rename("build/src/options/index.html"))
-  .pipe(gulp.dest('.'))
-;
-});
-
 gulp.task('sync-package-files', function() {
   return gulp.src(['bower.json', 'package.json'])
     .pipe(sync(syncOptions))
@@ -43,18 +27,23 @@ gulp.task('clean',function(cb) {
   del(['build',zipName], cb);
 })
 gulp.task('copy-non-minified', ['clean'], function() {
-  return gulp.src(['manifest.json','icons/*', 'src/injection/*.css',"src/shared/*.js"])
+  return gulp.src(['manifest.json','icons/*', 'src/injection/*.css',"src/shared/*.js", 'src/options/options.html'])
     .pipe(copy('build'));
 })
 gulp.task('build-injection', ['clean'], function() {
   return gulp.src('src/injection/*.js')
     .pipe(uglify())
     .pipe(gulp.dest('build/src/injection'));
-})
+});
+gulp.task('build-options', ['clean'], function() {
+  return gulp.src('src/options/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/src/options'));
+});
 gulp.task('zip', ['build'], function() {
   return gulp.src('build/**')
     .pipe(zip(zipName))
     .pipe(gulp.dest('.'));
 })
-gulp.task('build', ['build-injection','copy-non-minified','build-wc','rename-wc']);
+gulp.task('build', ['build-injection','build-options','copy-non-minified']);
 gulp.task('default',['build']);
